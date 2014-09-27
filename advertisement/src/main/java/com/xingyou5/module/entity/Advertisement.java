14dcac931b41421f73fd9d75b07421b0a2c20b56
weigongsheng <glacier_8400@126.com>
2014-09-28 00:01:54 +0800

    完成结算页面

---
+++ advertisement/src/main/java/com/xingyou5/module/entity/Advertisement.java
@@ -0,0 +1,51 @@
+package com.xingyou5.module.entity;
+
+import com.xingyou5.module.product.vo.Product;
+
+public class Advertisement {
+	private Integer id;
+	private String name;
+	private Product product;
+	private String picUrl;
+	private String targetUrl;
+	private String description;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public String getPicUrl() {
+		return picUrl;
+	}
+	public void setPicUrl(String picUrl) {
+		this.picUrl = picUrl;
+	}
+	public String getTargetUrl() {
+		return targetUrl;
+	}
+	public void setTargetUrl(String targetUrl) {
+		this.targetUrl = targetUrl;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
